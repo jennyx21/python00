72b8a8be7c8389fa6e6ce4cd1a3d3d34d8c9f565
jennyx21 <jtruckse@student.42heilbronn.de>
2026-02-24 19:02:04 +0100

    finish ex 6 and ex7

---
+++ ex7/ft_seed_inventory.py
@@ -0,0 +1,19 @@
+def ft_seed_inventory(a, b, c):
+    if c == "packets":
+        print(a, " seeds:", b, " Packages avalible")
+    elif c == "grams":
+        print(a, " seeds:", b, " grams total")
+    elif c == "area":
+        print(a, " seeds:", b, " square meters")
+    else:
+        print("no coorect unit found")
+
+
+# def main():
+#     ft_seed_inventory("tomato", 15, "packets")
+#     ft_seed_inventory("carrot", 8, "grams")
+#     ft_seed_inventory("lettuce", 12, "area")
+
+
+# if __name__ == "__main__":
+#     main()
