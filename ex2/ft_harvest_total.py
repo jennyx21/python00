7dde9b707af1a2324ff584844376609dd312094d
jennyx21 <jtruckse@student.42heilbronn.de>
2026-02-25 12:59:15 +0100

    fixed flake8 standarts

--- ex2/ft_harvest_total.py
+++ ex2/ft_harvest_total.py
@@ -1,5 +1,5 @@
 def ft_harvest_total():
-    day1=int(input("Day 1 Harvest: "))
-    day2=int(input("Day 2 Harvest: "))
-    day3=int(input("Day 3 Harvest: "))
+    day1 = int(input("Day 1 Harvest: "))
+    day2 = int(input("Day 2 Harvest: "))
+    day3 = int(input("Day 3 Harvest: "))
     print(f"Total Harvest: {day1 + day2 + day3}")
